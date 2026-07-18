47c5da3a00ec7cab67c42914efb1791823be8855
zqingquan <964005301@qq.com>
2026-07-19 00:54:24 +0800

    Do not run wallpaper update immediately on process start.

--- src/timetask.py
+++ src/timetask.py
@@ -1,7 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from src.wallpaper_update import run_wallpaper_update
 import sys
-import datetime
 from collections.abc import Callable
 from src.metadata.soft_config import DOWNLOAD_INTERVAL_TIME
 
@@ -9,12 +8,11 @@ from src.metadata.soft_config import DOWNLOAD_INTERVAL_TIME
 def stat_time_tast(pipeline: Callable[[], None]):
     scheduler = BlockingScheduler()
 
-    # 添加一个每隔一段时间执行一次
+    # 间隔触发；不设 next_run_time=now，避免启动时立刻下载更新
     scheduler.add_job(
         lambda: run_wallpaper_update(pipeline=pipeline, respect_pause=True),
         "interval",
         seconds=DOWNLOAD_INTERVAL_TIME,
-        next_run_time=datetime.datetime.now(),
     )
 
     # 也可以添加其他类型的任务，比如每天在特定时间执行
