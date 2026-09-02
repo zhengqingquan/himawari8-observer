6a1134d8d5f1df8ddc7c1a7f9032bf22660a511c
zhengqingquan <55186872+zhengqingquan@users.noreply.github.com>
2026-09-02 16:54:47 +0800

    Remove progressbar demo code tied to the deleted test sandbox.

--- src/tool/progressbar.py
+++ src/tool/progressbar.py
@@ -1,22 +1,6 @@
 """
 进度条
 """
-# from dl.dlpic import dl_pic_2
-
-url = "https://himawari8.nict.go.jp/img/D531106/1d/550/2021/05/21/012000_0_0.png"
-path = "../test/temp2.png"
-
-print("第一句话")
-# print("第二句话",end="\r")
-# print("第四句话",end="\r")
-print("\r第五句话", end="")
-print("\r第六句话", end="")
-print("第七句话")
-print("\r第八句话\r")
-print("\n第三句话")
-
-
-# dl_pic_2(url, path)
 
 
 def progressbar(filled, duration, frac, extra=""):
@@ -27,15 +11,3 @@ def progressbar(filled, duration, frac, extra=""):
         extra,
         end="",
     )
-
-
-# with alive_bar(3) as bar:
-#     time.sleep(3)
-#     bar()  # file read, tokenizing
-#     time.sleep(2)
-#     bar()  # tokens ok, processing
-#     time.sleep(5)
-#     bar()  # we're done! 3 calls with total=3
-
-# if __name__ == '__main__':
-#     pass
